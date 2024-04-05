05f96635bcd45a044f2f2ed28b4393f3c915ecfb
NatanLisboa <natanflisboa1@gmail.com>
2024-04-05 20:23:41 -0300

    chore!: Implement DDD classes

---
+++ domain/src/main/java/com/codeflix/admin/catalog/domain/Identifier.java
@@ -0,0 +1,5 @@
+package com.codeflix.admin.catalog.domain;
+
+public abstract class Identifier extends ValueObject {
+
+}
---
+++ domain/src/main/java/com/codeflix/admin/catalog/domain/ValueObject.java
@@ -0,0 +1,4 @@
+package com.codeflix.admin.catalog.domain;
+
+public abstract class ValueObject {
+}
